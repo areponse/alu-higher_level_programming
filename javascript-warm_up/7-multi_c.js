9a53c65d4246a3de702b3a061462da9ec5aaa2c1
Reponse Ashimwe <reponseashime@gmail.com>
2024-02-02 13:19:15 +0200

    Javascript - Warm up

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let a;
+if (!process.argv[2]) {
+  console.log("Missing number occurrences");
+} else {
+  a = process.argv[2];
+  while (a > 0) {
+    console.log(" C is Fun");
+    a--;
+  }
+}
